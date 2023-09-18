86394d417aa9e027abfb1d9b9108a0a4126c521f
leitongchen <chen@aprileconsulting.com>
2023-09-18 11:54:22 +0200

    Fix path to font and button

--- libs/ui-components/button/src/lib/button/button.component.ts
+++ libs/ui-components/button/src/lib/button/button.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'storybook-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent {}
+export class ButtonComponent {
+  private _type!: string;
+
+  @Input()
+  get type() {
+    return this._type;
+  }
+  set type(value) {
+    this._type = value;
+  }
+}
