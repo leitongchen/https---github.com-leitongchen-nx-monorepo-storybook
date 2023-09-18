bb14598f1617f063c54f8df98b352ee9bdff033b
leitongchen <chen@aprileconsulting.com>
2023-09-18 15:28:06 +0200

    Change storybook-host configurations - works

--- libs/ui-components/storybook-host/.storybook/main.ts
+++ libs/ui-components/storybook-host/.storybook/main.ts
@@ -1,15 +1,11 @@
-import type { StorybookConfig } from '@storybook/angular';
-
-const config: StorybookConfig = {
-  stories: ['../../**/src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
+module.exports = {
   framework: {
     name: '@storybook/angular',
     options: {},
   },
+  stories: ['../../**/src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
 };
-
-export default config;
 
 // To customize your webpack configuration you can use the webpackFinal field.
 // Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
