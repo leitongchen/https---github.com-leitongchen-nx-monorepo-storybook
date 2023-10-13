d3f99319f7dc69c3d0b0491f71c00b80c7898ca9
leitongchen <chen@aprileconsulting.com>
2023-10-13 14:01:53 +0200

    Add angular localize configuration
    Add dependencies for extract i18n and for merge them in one all.it.xlf file

--- apps/john/src/app/app.module.ts
+++ apps/john/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BannerModule } from '@storybook/banner';
+import { ButtonModule } from '@storybook/button';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [
     BrowserModule,
     BannerModule,
+    ButtonModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
