9568e1d9f5b429686b809d2d0401cc38b768ccb9
leitongchen <chen@aprileconsulting.com>
2023-10-16 12:55:20 +0200

    Add test runner configuration Add localize import to storybook-host

--- libs/ui-components/storybook-host/.storybook/main.ts
+++ libs/ui-components/storybook-host/.storybook/main.ts
@@ -6,7 +6,3 @@ module.exports = {
   stories: ['../../**/src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials'],
 };
-
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
